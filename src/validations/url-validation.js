829d39ceb5bf4d51cbba6689520d77bfa8f9d9fe
gauravghost <gyanendrak874@gmail.com>
2023-08-10 14:48:57 +0530

    Added url validation

--- src/validations/url-validation.js
+++ src/validations/url-validation.js
@@ -2,9 +2,11 @@ const Joi = require("joi");
 
 const create = {
     body: Joi.object().keys({
-        url: Joi.string().required(),
+        url: Joi.string().uri().required(),
     }),
 };
+
+
 
 module.exports = {
     create,
