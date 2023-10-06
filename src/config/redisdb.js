d4fed87527bc3db056d6318e0824006f91bede3e
gauravghost <gyanendrak874@gmail.com>
2023-10-06 12:20:34 +0530

    Fixed connectivity issue

--- src/config/redisdb.js
+++ src/config/redisdb.js
@@ -6,9 +6,6 @@ const client = redis.createClient({
   url: REDIS_URL,
   retry_strategy: function (options) {
     if (options.error && options.error.code === 'ECONNREFUSED') {
-      // End reconnecting on a specific error and flush all commands with
-      // a individual error
-      console.log(options);
       return new Error('The server refused the connection');
     }
     if (options.total_retry_time > 1000 * 60 * 60) {
